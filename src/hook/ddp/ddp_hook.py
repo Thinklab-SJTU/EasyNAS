b4cb24baf480bc1d9128eba8abdaea13e1aa6510
vicFigure <figure1_wxx@sjtu.edu.cn>
2023-08-01 15:22:10 +0800

    fix: debug for DDP

--- src/hook/ddp/ddp_hook.py
+++ src/hook/ddp/ddp_hook.py
@@ -19,6 +19,7 @@ class DDPHOOK(HOOK):
 #            if runner.val_loader.cfg.get('use_dist', True): runner.val_loader.sampler.set_epoch(runner.info.current_epoch)
 
     def after_train_epoch(self, runner):
+        #TODO: if EvalHOOK set only_master as True, the master process will be hung since synchronize_between_processes will barrier but no other process will conduct the barrier
         if runner.is_ddp():
             for k, v in runner.info.results.train.items():
                 runner.info.results.train[k] = synchronize_between_processes(v, device='cuda', mode='avg')
