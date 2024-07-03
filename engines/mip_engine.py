7b11f7418c5e94bd5c1fc821144207e394ac0298
vicFigure <figure1_wxx@sjtu.edu.cn>
2024-07-03 11:20:31 +0800

    update

--- engines/mip_engine.py
+++ engines/mip_engine.py
@@ -7,27 +7,31 @@ from src.hook import hooks_run, hooks_iter
 
 class MIPEngine(BaseEngine):
     def __init__(self, instance, solver, hooks=tuple()):
-        self.instance_loader, self.solver = self.build_from_cfg(instance, solver, hooks)
+#        self.instance_loader, self.solver = self.build_from_cfg(instance, solver, hooks)
+        self.build_from_cfg(instance, solver, hooks)
         self.info = edict({
             'results': edict({}), 
             })
 
-    def build_from_cfg(self, instance_cfg, solver_cfg, hooks_cfg):
+    def build_all(self, instance=None, solver=None, hooks=None):
         # build data
-        print("Building instance loader")
-        instance_loader = create_module(instance_cfg, search_path='src.mip')
+        if instance is not None:
+            print("Building instance loader")
+            self.instance_loader = create_module(instance, search_path='src.mip')
 
         # build model
-        print("Building mip solver")
-        solver = create_module(solver_cfg, search_path='src.mip')
+        if solver is not None:
+            print("Building mip solver")
+            self.solver = create_module(solver, search_path='src.mip')
 
         # build other hooks
-        print("Building hooks")
-        self._hooks = []
-        gen = hooks_cfg.values() if isinstance(hooks_cfg, dict) else iter(hooks_cfg)
-        for v in gen:
-            print(v)
-            self.register_hook(create_hook(v, search_path='src.mip'))
+        if hooks is not None:
+            print("Building hooks")
+            self._hooks = []
+            gen = hooks_cfg.values() if isinstance(hooks_cfg, dict) else iter(hooks_cfg)
+            for v in gen:
+                print(v)
+                self.register_hook(create_hook(v, search_path='src.mip'))
         return instance_loader, solver
 
     def run(self):
@@ -36,6 +40,12 @@ class MIPEngine(BaseEngine):
                 self.info.current_iter = i
                 with hooks_iter(self._hooks, self):
                     self.info.current_model = self.solver.solve(instance)
+
+    def update(self, sample):
+        self.build_all(**sample)
+        self.info = edict({
+            'results': edict({}), 
+            })
 
     def extract_performance(self):
         return self.info.results.get('best')
